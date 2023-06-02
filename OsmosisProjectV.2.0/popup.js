79c87bfe5e1a4d63fe68c2a721d1283f8287fe65
Paulo Castelo <paulo.castelo@fornecedores.sicoob.com.br>
2023-06-02 12:33:14 -0300

    Commit de alterações

--- OsmosisProjectV.2.0/popup.js
+++ OsmosisProjectV.2.0/popup.js
@@ -1,35 +1,24 @@
-// Record button logic
 let data = [];
 
+// Record button click event listener
 document.getElementById('record-btn').addEventListener('click', function() {
-  // Copy the page information
-  chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
-    chrome.tabs.sendMessage(tabs[0].id, { action: 'copyPageInfo' }, function(response) {
-      // Add the copied information to the data array
-      data.push(response);
-      console.log('Recorded page information:', data);
-    });
-  });
+  // Copy page information and store in background page
+  let pageInformation = document.documentElement.innerHTML;
+  data.push(pageInformation);
 });
 
-// Generate button logic
+// Generate button click event listener
 document.getElementById('generate-btn').addEventListener('click', function() {
-  // Create a new page with the stored information
-  let newTabUrl = chrome.extension.getURL('newtab.html');
-  chrome.tabs.create({ url: newTabUrl }, function(tab) {
-    // Send the stored information to the new page
-    chrome.tabs.sendMessage(tab.id, { action: 'loadPageInfo', data: data }, function(response) {
-      console.log('Generated new page with recorded information');
-      // Clear the data array and close the popup
-      data = [];
-      chrome.runtime.sendMessage('closePopup');
-    });
-  });
+  // Create new tab with page information
+  chrome.runtime.sendMessage({action: "createNewTab", information: data});
+  // Clear data
+  data = [];
 });
 
-// Close button logic
+// Close button click event listener
 document.getElementById('close-btn').addEventListener('click', function() {
-  // Clear the data array and close the popup
+  // Clear data
   data = [];
-  chrome.runtime.sendMessage('closePopup');
+  // Close popup
+  window.close();
 });
